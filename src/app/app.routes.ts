e764b2b98d30c478cb51befd44725a135c8aa8e3
Genn05 <ademjendoubiiii@gmail.com>
2024-11-18 20:14:31 +0100

    projet test angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'accueil',component:AccueilComponent},
+{path:'connexion',component:ConnexionComponent},
+{path:'insccription',component:InscriptionComponent},];
